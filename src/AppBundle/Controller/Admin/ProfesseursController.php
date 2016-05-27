37f98ad3fee16125135c1f6cb5caca4942e4e7cb
JeromeSnytsers <jerome.snytsers@epsi.fr>
2016-05-27 09:17:54 +0200

    ProfesseursController

---
+++ src/AppBundle/Controller/Admin/ProfesseursController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jérôme
+ * Date: 27/05/2016
+ * Time: 09:14
+ */
+class ProfesseursController extends Controller
+{
+    /**
+     * @Route("/admin/professeur", name="admin_professeur_homepage")
+     */
+    
+    
+}
